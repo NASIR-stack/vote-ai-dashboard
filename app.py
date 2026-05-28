d07544bc233616e6380a509586f2269c53556e30
NASIR-stack <70177891@student.uol.edu.pk>
2026-05-29 01:37:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import seaborn as sns
 # PAGE CONFIG
 # =========================
 st.set_page_config(
-    page_title="CYBERPUNK VOTE AI",
-    page_icon="⚡",
+    page_title="<VOTEGUARD AI",
+    page_icon="🛡️",
     layout="wide"
 )
 
