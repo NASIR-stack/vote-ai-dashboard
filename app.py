3b197a23979983488d2ba696141dc3c609fbcd4e
NASIR-stack <70177891@student.uol.edu.pk>
2026-05-29 01:51:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -379,7 +379,16 @@ if question:
         st.sidebar.error(
             f"🚨 Highest risk candidate: {risky_candidate}"
         )
+# =========================
+# DOWNLOAD DATA BUTTON
+# =========================
 
+csv = filtered_df.to_csv(index=False)
+
+st.download_button(
+    label="📥 Download The Data",
+    data=csv,
+   
     # =========================
     # DEFAULT RESPONSE
     # =========================
